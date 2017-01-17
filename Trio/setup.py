aa43099751d88f3c1097d4f99414d0cdacd48fda
baraza moses <barazayo@gmail.com>
2017-01-17 10:26:51 +0300

    Add files via upload

---
+++ Trio/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='trio',
+      version='0.1',
+      description='know your name',
+      author='barazayo!',
+      url='',
+      author_email='barazayo@gmail.com',
+      license='MIT',
+      packages=['trio'],
+      zip_safe=False)
---
+++ Trio/trio/trio.py
@@ -0,0 +1,10 @@
+class Trio():
+    def __init__(self, name, age):
+        self.name=name
+        self.age=age
+
+    def print_my_name(self):
+        print("Your name is ", self.name)
+    def print_my_age(self):
+        print("You are ", self.age, "years old.")
+
